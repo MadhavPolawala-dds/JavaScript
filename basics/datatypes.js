cf6bfb05960487be16cc813c65f946781796eaf5
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-08-14 10:43:48 +0530

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,5 @@
+let age = 22;
+
+console.log(
+  typeof "Madhav" + " " + typeof 22 + " " + typeof true + " " + typeof age
+);
---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+const id = 123;
+let Name = "Madhav";
+var lastName = "Polawala";
+email = "madhav@test.com";
+
+Name = "Praveen";
+lastName = "Kumar";
+email = "praveen@test.com";
+
+console.log(id + " " + Name + " " + lastName + " " + email);
