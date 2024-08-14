cd3d6eafe373939749aa93812a46d5d6563e8028
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-08-14 11:32:16 +0530

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,6 @@
+console.log("4" < "7");
+console.log(2 > 3);
+console.log("2" == 2); //not recommended
+console.log("2" < 1); //not recommended
+console.log("2" > 1); //not recommended
+console.log("2" === 2); 
