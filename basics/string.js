d3f62d07c4a94c3e7bb7d4b42e650e5d0023ced6
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-08-14 12:54:08 +0530

    string

---
+++ basics/string.js
@@ -0,0 +1,11 @@
+let name = "Madhav";
+let age = 22;
+let newName = new String("MadhavPolawala");
+
+console.log(`Hello, My name is ${name.toUpperCase()} and my age is ${age}`);
+
+console.log(newName[6]);
+console.log(newName.length);
+console.log(newName.charAt(10));
+console.log(newName.toLowerCase());
+console.log(newName.__proto__);
