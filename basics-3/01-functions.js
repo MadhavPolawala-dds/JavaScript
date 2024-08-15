535e8c1673d44fe6c922ce9542631826283b222b
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-08-15 10:02:09 +0530

    function continue

--- basics-3/01-functions.js
+++ basics-3/01-functions.js
@@ -6,4 +6,11 @@ function userFunction(username = "Madhav") {
   return `${username} just logged in.`;
 }
 
-console.log(userFunction("Polawala"));
+// console.log(userFunction("Polawala"));
+
+const myArr = [100, 200, 300, "Madhav", "Polawala"];
+
+function getValue(arr) {
+  return arr[4];
+}
+console.log(getValue(myArr));
